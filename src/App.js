e38a4ded08aabc7ee3c4f675fa72213b7dc642ea
Heather Hartley <hlhartley@gmail.com>
2019-04-26 15:15:47 -0600

    Displays title and explanation

--- src/App.js
+++ src/App.js
@@ -2,24 +2,34 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-
   const [picOfTheDay, setPic] = useState([]);
-  console.log('pic', picOfTheDay)
+  var imageStyle = {
+    height: "100vh",
+    backgroundImage: "url(" + ` ${picOfTheDay.hdurl}` + ")",
+    backgroundSize: "100% 100%",
+    color: "white"
+  };
   
   useEffect(() => {
     async function fetchPic() {
-      const url = `https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_API_KEY}`;
-      const response = await fetch(url)
-      const result = await response.json()
-      setPic(result)
-      console.log(result)
+      try {
+        const url = `https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_API_KEY}`;
+        const response = await fetch(url)
+        const result = await response.json()
+        setPic(result)
+      } catch (error) {
+        console.log(error)
+      }
     }
     fetchPic()
   }, []);
 
   return (
     <div className="App">
-      App
+      <div style={imageStyle}>
+        <h1>{`${picOfTheDay.title}`}</h1>
+        <p>{`${picOfTheDay.explanation}`}</p>
+      </div>
     </div>
   );
 }
