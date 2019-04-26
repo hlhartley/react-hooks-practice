22af01cd8338fe395b91ee21d8ab42b9b5d340f4
Heather Hartley <hlhartley@gmail.com>
2019-04-26 14:55:40 -0600

    Adds useState and useEffect hooks in App component and makes fetch call to nasa api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [picOfTheDay, setPic] = useState([]);
+  console.log('pic', picOfTheDay)
+  
+  useEffect(() => {
+    async function fetchPic() {
+      const url = `https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_API_KEY}`;
+      const response = await fetch(url)
+      const result = await response.json()
+      setPic(result)
+      console.log(result)
+    }
+    fetchPic()
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      App
     </div>
   );
 }
